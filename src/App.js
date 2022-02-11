7e456f616acaacbd20698e2cfbbce6562dcb9505
Thiyophin Johnson <thiyophinj99@gmail.com>
2022-02-11 22:42:02 +0530

    top rated added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import Navbar from './Components/Navbar/Navbar';
 import './App.css'
 import Banner from './Components/Banner/Banner';
 import RowPosts from './Components/RowPosts/RowPosts';
-import {action,originals,upcoming} from './urls'
+import {action,originals,upcoming,toprated} from './urls'
 function App() {
   return (
     <div className="App">
      <Navbar />
      <Banner />
      <RowPosts url={originals} title="Netflix Originals"/>
-     <RowPosts url={upcoming} title="Upcoming movies" isSmall/>
+     <RowPosts url={upcoming} title="Upcoming movies" />
      <RowPosts url={action} title="Action movies" isSmall/>
+     <RowPosts url={toprated} title="Top Rated movies" isSmall/>
     </div>
   );
 }
--- src/urls.js
+++ src/urls.js
@@ -2,3 +2,4 @@ import {API_KEY} from './Constants/Constants'
 export const originals = `discover/tv?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate`;
 export const action = `discover/movie?api_key=${API_KEY}&with_genres=28`;
 export const upcoming = `movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`;
+export const toprated = `movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`;
